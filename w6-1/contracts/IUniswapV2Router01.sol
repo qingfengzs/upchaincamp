a8e62cdb63f240e0004460756f8c844d0214d60d
jinyu <jinyu1@zhaohu365.cn>
2023-04-21 16:02:23 +0800

    w6-1 init

--- w6-1/contracts/IUniswapV2Router01.sol
+++ w6-1/contracts/IUniswapV2Router01.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity =0.8.13;
+pragma solidity >=0.6.2;
 
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
